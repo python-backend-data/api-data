c1a1dd860ffa96e04e7b571618b7d63c1e436ab2
Luis <luis@10.10.5.119>
2024-07-20 07:36:14 -0500

    primera ruta

--- src/api/app.py
+++ src/api/app.py
@@ -1,6 +1,15 @@
 """
 Servidor
 """
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/data")
+def get_data():
+    return jsonify({"data" : "data"})
+
 
 def main():
     return 123
